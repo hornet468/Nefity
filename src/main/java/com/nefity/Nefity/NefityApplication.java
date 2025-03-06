4355906e2af982bce6aa839b31d31ecc7148d620
hornet468 <tomhelington@gmail.com>
2025-03-06 17:20:16 +0000

    first commit

---
+++ src/main/java/com/nefity/Nefity/NefityApplication.java
@@ -0,0 +1,13 @@
+package com.nefity.Nefity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NefityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NefityApplication.class, args);
+	}
+
+}
