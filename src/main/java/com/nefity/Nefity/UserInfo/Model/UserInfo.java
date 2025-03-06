4db489de17d29b92f8a84440764835d0b1ead9df
What`s u name? MADA*** <kakapo716@gmail.com>
2025-03-06 20:33:52 +0200

    init userInfo

---
+++ src/main/java/com/nefity/Nefity/UserInfo/Model/UserInfo.java
@@ -0,0 +1,20 @@
+package com.nefity.Nefity.UserInfo.Model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class UserInfo {
+
+    private String firstName;
+    private String lastName;
+    private String nickName;
+    private String Email;
+    private String Country;
+    private LocalDate dateOfBirth;
+    private String categoryProfile;
+    private int age;
+
+
+}
