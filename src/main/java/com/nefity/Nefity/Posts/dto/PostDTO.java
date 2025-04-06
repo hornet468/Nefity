8e7afa0e305843cbc2a5d5555e94f4cd8b1e06a4
hornet468 <tomhelington@gmail.com>
2025-04-06 14:25:33 +0000

    Basic Data Transfer Object v1.1

--- src/main/java/com/nefity/Nefity/Posts/dto/PostDTO.java
+++ src/main/java/com/nefity/Nefity/Posts/dto/PostDTO.java
@@ -1,4 +1,17 @@
 package com.nefity.Nefity.Posts.dto;
 
+import com.nefity.Nefity.UserInfo.dto.UserInfoDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class PostDTO {
+    private Long id;
+    private String title;
+    private String text;
+    private LocalDateTime timestamp;
+    private UserInfoDTO user;
 }
