33424f3b255776c3709f20529a22a7d5d1e7a96b
hornet468 <tomhelington@gmail.com>
2025-04-07 19:06:19 +0000

    Starting LikeDTO

---
+++ src/main/java/com/nefity/Nefity/Likes/dto/LikeDTO.java
@@ -0,0 +1,15 @@
+package com.nefity.Nefity.Likes.dto;
+
+import com.nefity.Nefity.UserInfo.dto.UserInfoDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class LikeDTO {
+    private long id;
+    private LocalDateTime timestamp;
+    private UserInfoDTO user;
+}
--- src/main/java/com/nefity/Nefity/UserInfo/Controller/UserInfoController.java
+++ src/main/java/com/nefity/Nefity/UserInfo/Controller/UserInfoController.java
@@ -46,3 +46,5 @@ public class UserInfoController {
         return service.addUserInfo(User);
     }
 }
+
+
