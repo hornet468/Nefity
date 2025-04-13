aa38702af00e5617d42aff3bd6d3e9e6b29979ef
hornet468 <tomhelington@gmail.com>
2025-04-13 13:15:31 +0000

    refactoring mapper for user and add likes and posts mapper

--- src/main/java/com/nefity/Nefity/UserInfo/mapper/UserInfoMapper.java
+++ src/main/java/com/nefity/Nefity/UserInfo/mapper/UserInfoMapper.java
@@ -3,11 +3,13 @@ package com.nefity.Nefity.UserInfo.mapper;
 import com.nefity.Nefity.UserInfo.Model.UserInfo;
 import com.nefity.Nefity.UserInfo.dto.UserInfoDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserInfoMapper {
     UserInfoDTO toDTO(UserInfo userInfo);
+
     List<UserInfoDTO> toDTOList(List<UserInfo> userInfoList);
 }
